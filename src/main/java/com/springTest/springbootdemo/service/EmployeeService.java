16abb2ac50d81fbca1565fb447a7eba44b6005fb
PranavSagar <pranav.sagar@outlook.com>
2023-08-15 23:25:27 +0530

    employee model with APIs

---
+++ src/main/java/com/springTest/springbootdemo/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.springTest.springbootdemo.service;
+
+import com.springTest.springbootdemo.model.Employee;
+
+public interface EmployeeService {
+
+    Employee save(Employee employee);
+}
